9b5a67ac4a584786c85702fd8f44b2780c39cb1e
Heidi Dodge <heidi.maria.93@hotmail.com>
2024-12-22 19:30:05 -0330

    Added Button Control Keys

--- input.js
+++ input.js
@@ -1,23 +1,40 @@
 const { connect } = require('./client');
 
-const setupInput = function () {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume();
-    stdin.on("data", handleUserInput);
-    return stdin;
-  };
-  
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
   const handleUserInput = function (key) {
     if (key === "\u0003") {
       process.exit();
-    }
-  };
-  
-  connect();
-  setupInput();
+    };
 
-  module.exports = {
-    setupInput
+  switch (key) {
+    case 'w':
+      connection.write("Move: up");
+      break;
+    case 'a':
+      connection.write("Move: left");
+      break;
+    case 's':
+      connection.write("Move: down");
+      break;
+    case 'd':
+      connection.write("Move: right");
+      break;
+    default:
+      console.log("Invalid key pressed");
+  }
+};
+
+module.exports = {
+  setupInput,
 };
