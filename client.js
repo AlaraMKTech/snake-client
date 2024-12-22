6cd69a47674303ae0538b4d1eade3bd93996fb21
Heidi Dodge <heidi.maria.93@hotmail.com>
2024-12-22 19:29:22 -0330

    Refactoring

--- client.js
+++ client.js
@@ -1,11 +1,5 @@
 const net = require("net");
 
-// Movement Commands
-//"Move: up" - move up one square (unless facing down)
-//"Move: down" - move down one square (unless facing up)
-//"Move: left" - move left one square (unless facing right)
-//"Move: right" - move left one square (unless facing left)
-
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
@@ -13,29 +7,20 @@ const connect = function () {
     port: 50541,
   });
 
-  // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  // handles incoming data and logs it for the player
   conn.on("data", (data) => {
     console.log("Incoming data: ", data);
   });
 
   conn.on("connect", () => {
     console.log("Successfully connected to game server.");
-
     conn.write("Name: HMD");
-
-    conn.write("Move: up");
   });
-
 
   return conn;
 };
 
-console.log("Connecting ...");
-connect();
-
 module.exports = {
-    connect
+  connect
 };
