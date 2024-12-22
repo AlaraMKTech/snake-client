added80aa3341eee4cbcb50a7e7e88f332c7f005
Heidi Dodge <heidi.maria.93@hotmail.com>
2024-12-22 17:19:43 -0330

    Removed connect function, and exported it into play.js

--- play.js
+++ play.js
@@ -1,22 +1,2 @@
-const net = require("net");
+const { connect } = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost"
-    port: 4455
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // handles incoming data and logs it for the player
-  conn.on("data", (data) => {
-    console.log("Incoming data: ", data);
-  });
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
