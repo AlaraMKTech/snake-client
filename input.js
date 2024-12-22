74057305f4ab8bd0bd6e075ab9d731c6f406a027
Heidi Dodge <heidi.maria.93@hotmail.com>
2024-12-22 19:43:51 -0330

    Added Pre-Written Message Commands

--- input.js
+++ input.js
@@ -21,15 +21,39 @@ const setupInput = function (conn) {
     case 'w':
       connection.write("Move: up");
       break;
+
     case 'a':
       connection.write("Move: left");
       break;
+
     case 's':
       connection.write("Move: down");
       break;
+
     case 'd':
       connection.write("Move: right");
       break;
+
+    case '1':
+      connection.write("Say: Hey there!");
+      break;
+
+    case '2':
+      connection.write("Say: Good luck!(Not really)");
+      break;
+
+    case '3':
+      connection.write("Say: How did you get so good??");
+      break;
+
+    case '4':
+      connection.write("Say: I am the BEST!!");
+      break;
+
+    case '5':
+      connection.write("Say: Victory is MINE!");
+      break;
+
     default:
       console.log("Invalid key pressed");
   }
